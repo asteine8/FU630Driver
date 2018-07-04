57e534080b9fbc501f87bd8b3e95d34a9f3df7a0
Phillia <37705707+psteine3@users.noreply.github.com>
2018-07-04 10:26:02 -0700

    Added git commands to switch to this branch in shell script

--- prepForTesting.sh
+++ prepForTesting.sh
@@ -1,6 +1,12 @@
-#!~\Documents
-echo $PATH
+#!~\Documents\FU630Driver
+echo Changing git branch to gradiantOptimization
+git pull
+git status
+sleep 1
+git checkout gradiantOptimization .
+git pull -v
+sleep 1
 echo Moving OptDriverTst.py outside of driver folder
-cp -f -v FU630Driver\OptDriverTst.py
 cd ~\Documents
+cp -f -v FU630Driver\OptDriverTst.py OptDriverTst.py
 rm -i -v OptDriverTstTst.py
